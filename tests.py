3e3dd40b4b87529a1f8f62e9c2d5a1c26368b2ea
jacobread <jread8888@gmail.com>
2022-11-08 18:44:52 -0700

    Updated tests. Finished HW3

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 from consumer import *
 import unittest
+import boto3
+import bucket
+import requests
 
 s3 = boto3.resource('s3')
 client = boto3.client("s3")
@@ -13,14 +16,14 @@ class TestConsumer(unittest.TestCase):
 
     def test_getSmallestKey(self):
         keys = ["1612306368338", "1612306368339", "1612306368340"]
-        smallestKey, keyList, filename = getSmallestKey(keys)
+        smallestKey, keyList, filename = bucket.getSmallestKey(keys)
         self.assertEqual(smallestKey, "1612306368338")
 
     def test_getAllKeys(self):
         client.upload_file("sample-requests/1612306368338", bucket2Name, "1612306368338")
         client.upload_file("sample-requests/1612306369227", bucket2Name, "1612306369227")
         client.upload_file("sample-requests/1612306369451", bucket2Name, "1612306369451")
-        keys = getAllKeys(bucket2)
+        keys = bucket.getAllKeys(bucket2)
 
         for key in keys:
             s3.Object(bucket2Name, key).delete()
@@ -36,10 +39,10 @@ class TestConsumer(unittest.TestCase):
         bucket3.objects.filter(Prefix="widgets/").delete()
 
         client.upload_file("sample-requests/1612306368338", bucket2Name, "1612306368338")
-        getRequest(bucket2)
-        object = getAllKeys(bucket3)
+        getRequest(bucket2, None)
+        object = bucket.getAllKeys(bucket3)
 
-        if object == None:
+        if (object == None or len(object) == 0):
             self.fail("No objects in bucket 2")
         
         self.assertEqual(object[0], "widgets/mary-matthews/8123f304-f23f-440b-a6d3-80e979fa4cd6")
@@ -51,21 +54,50 @@ class TestConsumer(unittest.TestCase):
             "owner": "Mary Matthews",
             "widgetId": "8123f304-f23f-440b-a6d3-80e979fa4cd6",
         }
-        createRequest(dictionary, True)
-        object = getAllKeys(bucket3)
+        requests.createRequest(dictionary, "s3")
+        object = bucket.getAllKeys(bucket3)
 
         if object == None:
             self.fail("No objects in bucket 3")
 
         self.assertEqual(object[0], "widgets/mary-matthews/8123f304-f23f-440b-a6d3-80e979fa4cd6")
 
-    # def test_deleteRequest(self):
-    #     deleteRequest()
-    #     self.assertTrue(True)
+    def test_deleteRequest(self):
+        bucket2.objects.all().delete()
+        bucket3.objects.filter(Prefix="widgets/").delete()
+        dictionary = {
+            "owner": "Mary Matthews",
+            "widgetId": "8123f304-f23f-440b-a6d3-80e979fa4cd6",
+        }
+        requests.createRequest(dictionary, "s3")
+        if object == None:
+            self.fail("No objects in bucket 3 so I cannot proceed to delete the object")
 
-    # def test_changeRequest(self):
-    #     changeRequest()
-    #     self.assertTrue(True)
+        requests.deleteRequest("s3", dictionary)
+
+        keys = bucket.getAllKeys(bucket2)
+        self.assertEqual(len(keys), 0)
+
+    def test_changeRequest(self):
+        bucket2.objects.all().delete()
+        bucket3.objects.filter(Prefix="widgets/").delete()
+        dictionary = {
+            "owner": "Mary Matthews",
+            "widgetId": "8123f304-f23f-440b-a6d3-80e979fa4cd6",
+            "type": "update",
+            "info": "Here is some information"
+        }
+        requests.createRequest(dictionary, "s3")
+        if object == None:
+            self.fail("No objects in bucket 3 so I cannot proceed to update the object")
+
+        response = requests.updateRequest("dynamo", dictionary)
+
+        if (response):
+            self.assertEqual(type(response), dict)
+        else:
+            self.fail("Failed to update dynamodb table")
+
 
 if __name__ == '__main__':
     unittest.main()
